6630e94f71c2afc90d5647ffc24fb582707e54e2
dylangroos <hawkins.groos@gmail.com>
2023-04-17 15:51:29 -0400

    ayo

--- gsheet/constants.py
+++ gsheet/constants.py
@@ -2,29 +2,29 @@ SCOPE = ['https://www.googleapis.com/auth/spreadsheets']
 
 SHEET_ID = '18Ln_q5fxjasOFD-_aORYFK6HynpreB_ivaG0W63JDK8'
 FORWARD_RANGES = {
-    "names": "Home Page Test!A14:A100",
-    "goals": "Home Page Test!B14:B100",
-    "assists": "Home Page Test!C14:C100",
-    "plus_minus": "Home Page Test!D14:D100",
-    "ot_goals": "Home Page Test!E14:E100",
-    "fights": "Home Page Test!F14:F100",
-    "all": "Home Page Test!A14:F100"
+    "names": "Home Page Test!A14:A150",
+    "goals": "Home Page Test!B14:B150",
+    "assists": "Home Page Test!C14:C150",
+    "plus_minus": "Home Page Test!D14:D150",
+    "ot_goals": "Home Page Test!E14:E150",
+    "fights": "Home Page Test!F14:F150",
+    "all": "Home Page Test!A14:F150"
 }
 DEFENSE_RANGES = {
-    "names": "Home Page Test!H14:H50",
-    "goals": "Home Page Test!I14:I50",
-    "assists": "Home Page Test!J14:J50",
-    "plus_minus": "Home Page Test!K14:K50",
-    "ot_goals": "Home Page Test!L14:L50",
-    "fights": "Home Page Test!M14:M50",
-    "all": "Home Page Test!H14:M50"
+    "names": "Home Page Test!H14:H100",
+    "goals": "Home Page Test!I14:I100",
+    "assists": "Home Page Test!J14:J100",
+    "plus_minus": "Home Page Test!K14:K100",
+    "ot_goals": "Home Page Test!L14:L100",
+    "fights": "Home Page Test!M14:M100",
+    "all": "Home Page Test!H14:M100"
 }
 GOALIE_RANGES = {
-    "names": "Home Page Test!O14:O30",
-    "wins": "Home Page Test!P14:P30",
-    "losses": "Home Page Test!Q14:Q30",
-    "shutouts": "Home Page Test!R14:R30",
-    "saves": "Home Page Test!S14:S30",
-    "goals_against": "Home Page Test!T14:T30",
-    "all": "Home Page Test!O14:T30"
+    "names": "Home Page Test!O14:O40",
+    "wins": "Home Page Test!P14:P40",
+    "losses": "Home Page Test!Q14:Q40",
+    "shutouts": "Home Page Test!R14:R40",
+    "saves": "Home Page Test!S14:S40",
+    "goals_against": "Home Page Test!T14:T40",
+    "all": "Home Page Test!O14:T40"
 }
